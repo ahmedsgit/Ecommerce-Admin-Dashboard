00eff9ee0ac8f14d0906cc88a3c3553dd2230ee5
ahmedsgit <acc.ahmedsazzad@gmail.com>
2022-06-07 13:13:39 +0600

    Navigation bar w/ resize widths

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,12 @@ import { useStateContext } from '../contexts/ContextProvider';
 import { links } from '../data/dummy';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
   const activeLink =
     'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink =
@@ -19,7 +24,7 @@ const Sidebar = () => {
           <div className='flex justify-between items-center'>
             <Link
               to='/'
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className='flex items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight dark:text-white text-slate-800'
             >
               <SiShopware /> <span>Fit Life</span>
@@ -28,7 +33,7 @@ const Sidebar = () => {
               <button
                 type='button'
                 onClick={() => setActiveMenu(prevActiveMenu => !prevActiveMenu)}
-                className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
+                className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block'
               >
                 <MdOutlineCancel />
               </button>
@@ -42,7 +47,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSidebar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,17 +1,34 @@
 import { createContext, useContext, useState } from 'react';
 
 const Context = createContext();
-// const initialState = {
-//   chat: false,
-//   cart: false,
-//   useProfile: false,
-//   notification: false,
-// };
+const initialState = {
+  chat: false,
+  cart: false,
+  useProfile: false,
+  notification: false,
+};
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = clicked => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
   return (
-    <Context.Provider value={{ activeMenu, setActiveMenu }}>
+    <Context.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
       {children}
     </Context.Provider>
   );
